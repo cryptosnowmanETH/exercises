fd49e83d6aca59d73cac78302606d1b703fcb27e
Dmitriy Azarenko <dazarenko3@gmail.com>
2022-12-23 01:47:22 -0500

    password generator

--- passwordgenerator.py
+++ passwordgenerator.py
@@ -9,3 +9,24 @@ Extra:
 
 Ask the user how strong they want their password to be. For weak passwords, pick a word or two from a list."""
 
+import random
+randomthing = random.seed(a=None, version=2)
+#print(randomthing)
+
+#shuffle = random.shuffle(1[1,2])
+#print(shuffle)
+
+def password_generator():
+    password_strength = input("How strong do you want your password to be? Weak, Medium, Strong ")
+    password_length = 0
+    if password_strength == "Weak":
+        password_length = 6
+    elif password_strength == "Medium":
+        password_length = 10
+    else:
+        password_length = 15
+    sample = random.sample("abcdetfghijk1mnopqrstuvwxyz1234567890!@#$%^&*()", password_length)
+    sample_password = "".join(sample)
+    return sample_password
+
+print(password_generator())
